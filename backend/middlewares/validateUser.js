cdd104f89ae2eb44be42e77065c94c26b1b51b1a
karel <karelgeyer@gmail.com>
2021-10-29 01:21:29 +0100

    fix: use correct indentation

--- backend/middlewares/validateUser.js
+++ backend/middlewares/validateUser.js
@@ -15,10 +15,10 @@ const validateUser = async (req, res, next) => {
     const isPasswordValid = await bcrypt.compare(req.body.password, user.password);
 
     if (!isPasswordValid) {
-      return res.status(401).send('password or email is wrong')
+      return res.status(401).send('password or email is wrong');
     };
 
-    req.thisUser = user
+    req.thisUser = user;
     next();
   } catch (err) {
     return res.status(400).send(err);
