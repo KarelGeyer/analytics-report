c62ae8cada12db2b5b6b35b84a2c73119b38f668
karel <karelgeyer@gmail.com>
2021-11-03 23:29:17 +0000

    feat: create new user related reducer and actions
    create a new reducer to manage user related data, function is not
    fully done yet as it needs to be enhanced and take secruity into
    an account

---
+++ frontend/src/state/reducers.js
@@ -0,0 +1,32 @@
+import { USER_ACTIONS } from './actions'
+import { STATIC_DATA } from '../assets/static_data/StaticData'
+import axios from 'axios';
+
+const { ADD_USER, UPDATE_USER, DELETE_USER } = USER_ACTIONS
+
+export const userReducer = (user, action) => {
+  const { payload } = action;
+
+  switch (action.type) {
+    case UPDATE_USER:
+      axios.patch(STATIC_DATA.URL.fetchUsers, payload)
+        .then(res => {
+          console.log(res)
+        })
+        .catch((err) => { console.log(err) })
+
+      break;
+
+    case DELETE_USER:
+      axios.delete(STATIC_DATA.URL.fetchUsers, { data: payload })
+        .then(res => {
+          console.log(res)
+        })
+        .catch((err) => { console.log(err) })
+
+      break;
+
+    default:
+      return
+  }
+}
